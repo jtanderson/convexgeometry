572236e7f7374eff309391f65a3b91dcdc9c31ca
Joe Anderson <jtanderson@salisbury.edu>
2021-06-07 11:44:02 -0400

    Work on non-ellipsoidal separation

--- convexgeometry/optimization.py
+++ convexgeometry/optimization.py
@@ -83,18 +83,21 @@ def WSep2Viol(c, gamma, epsilon, n, R, sepK):
         # so we assert that c.x < gamma + eps
         return True
 
-def WMem2Viol(y, delta, n, R, r, a0):
+# TODO -- Theorem 4.3.2
+def WMem2Viol(c, gamma, eps, memK, n, R, r, a0):
     """
     Given a body (K; n, R, r, a0), where
+    memK: weak-membership oracle
     n: dimensions
     R: circumradius so S(0,r) contains K
     r: radius so that of S(a0,r) in K
     Either
-       i) asserts that y in S(K,delta)
-      ii) asserts that y not in S(K, -delta)
+       i) asserts that c.T @ x <= gamma + eps for all x in in S(K,-eps)
+      ii) find a vector y in S(K, eps) with c.T @ x >= gamma - eps
     """
     pass
 
+# Lemma 4.3.3
 def StrongerMem(y, delta, memK, n, R, r, a):
     """
     Given vector y, error delta > 0,
@@ -118,11 +121,52 @@ def StrongerMem(y, delta, memK, n, R, r, a):
     assert(isinstance(test,bool))
     return test
 
+# Lemma 4.3.4
+def SeparationNonEllipsoid(y, delta, beta, memK, n, R, r, a):
+    """
+    Either
+       i) asserts that y in S(K,delta),
+      ii) finds vector c such that c != 0 and for every x in K,
+          c.T @ x <= c.T @ y + (delta + beta |x-y|) |c|
+    """
+    if la.norm(y - a) >= 2*R:
+        return (y-a) / la.norm(y - a)
+
+    if StrongerMem(y, delta, memK, n, R, r, a):
+        return True
+
+    # StrongerMem says y not in K
+    alpha = np.arctan(beta/(4*n*n))
+    delta1 = (r*delta) / (R + r)
+    r1 = (r*delta1) / (4*n*R)
+    eps1 = (beta*beta*r1) / (16*n*n*n*n)
+
+    in_v = np.copy(a)
+    out_v = np.copy(y)
+
+    # binary search to straddle the boundary close enough
+    while la.norm(in_v - out_v) > delta1/(2*n):
+        new_v = (in_v + out_v) / 2
+        if memK(new_v, eps1, n, R):
+            in_v = np.copy(new_v)
+        else:
+            out_v = np.copy(new_v)
+
+    vpp = (1/(r + eps1)) * ((r-r1)*in_v + (r1 + eps1)*a)
+
+    # we know S(vpp, r1) subseteq K
+    # now need to re-center the algorithm at vpp so that vpp == 0...
+    # TODO finish, page 109
+
+
+# TODO
 def WVal():
     pass
 
+# TODO
 def WSep():
     pass
 
+# TODO
 def Wmem():
     pass
